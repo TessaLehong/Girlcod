f134322651c9dcd41890829415d61572721c7f2f
TessaLehong <136630270+TessaLehong@users.noreply.github.com>
2024-08-15 18:33:45 +0200

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,12 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const rememberMe = document.getElementById('rememberMe').checked;
+
+    console.log('Email:', email);
+    console.log('Password:', password);
+    console.log('Remember Me:', rememberMe);
+
+    // Add your login logic here
+});
